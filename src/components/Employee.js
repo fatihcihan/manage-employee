07e8d944a8bbf0ba07f49d2f696ba6da16afd661
Fatih <fatihcihan666@gmail.com>
2023-04-28 17:38:32 +0300

    Adjusted the href attribute to avoid warning

--- src/components/Employee.js
+++ src/components/Employee.js
@@ -10,8 +10,8 @@ const Employee = ({ employees }) => {
                         <td>{employee.address}</td>
                         <td>{employee.phone}</td>
                         <td>
-                            <a href="" className="edit" data-toggle="modal"><i className="material-icons" data-toggle="tooltip" title="Edit">&#xE254;</i></a>
-                            <a href="" className="delete" data-toggle="modal"><i className="material-icons" data-toggle="tooltip" title="Delete">&#xE872;</i></a>
+                            <a href="/" className="edit" data-toggle="modal"><i className="material-icons" data-toggle="tooltip" title="Edit">&#xE254;</i></a>
+                            <a href="/" className="delete" data-toggle="modal"><i className="material-icons" data-toggle="tooltip" title="Delete">&#xE872;</i></a>
                         </td>
                     </tr>
                 ))
