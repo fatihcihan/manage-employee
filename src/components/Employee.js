99f4c78146ebec77f71b917215b22bdce02dba6b
Fatih <fatihcihan666@gmail.com>
2023-04-28 16:32:06 +0300

    Data from Employee list displayed on screen

--- src/components/Employee.js
+++ src/components/Employee.js
@@ -1,16 +1,22 @@
 
-const Employee = () => {
+const Employee = ({ employees }) => {
     return (
-        <tr>
-            <td>Thomas Hardy</td>
-            <td>thomashardy@mail.com</td>
-            <td>89 Chiaroscuro Rd, Portland, USA</td>
-            <td>(171) 555-2222</td>
-            <td>
-                <a href="" className="edit" data-toggle="modal"><i className="material-icons" data-toggle="tooltip" title="Edit">&#xE254;</i></a>
-                <a href="" className="delete" data-toggle="modal"><i className="material-icons" data-toggle="tooltip" title="Delete">&#xE872;</i></a>
-            </td>
-        </tr>
+        <>
+            {
+                employees.map((employee) => (
+                    <tr key={employee.id}>
+                        <td>{employee.name}</td>
+                        <td>{employee.email}</td>
+                        <td>{employee.address}</td>
+                        <td>{employee.phone}</td>
+                        <td>
+                            <a href="" className="edit" data-toggle="modal"><i className="material-icons" data-toggle="tooltip" title="Edit">&#xE254;</i></a>
+                            <a href="" className="delete" data-toggle="modal"><i className="material-icons" data-toggle="tooltip" title="Delete">&#xE872;</i></a>
+                        </td>
+                    </tr>
+                ))
+            }
+        </>
     )
 }
 
