42e8a166b33aace2c75e8597a3db52144854844e
Fatih <fatihcihan666@gmail.com>
2023-04-30 03:31:16 +0300

    Use context is used, input changes are monitored with the help of onInputChange method

--- src/components/AddForm.js
+++ src/components/AddForm.js
@@ -1,37 +1,76 @@
-import { Form } from 'react-bootstrap';
+import { Form, Button } from 'react-bootstrap';
+import { EmployeeContext } from '../contexts/EmployeeContext';
+import { useContext, useState } from 'react';
 
 const AddForm = () => {
 
+    const { addEmployee } = useContext(EmployeeContext);
+
+    /*  const [name, setName] = useState("");
+     const [email, setEmail] = useState("");
+     const [address, setAddress] = useState("");
+     const [phone, setPhone] = useState(""); */
+
+    const [newEmployee, setNewEmployee] = useState({
+        name: "",
+        email: "",
+        address: "",
+        phone: ""
+    });
+
+    const { name, email, address, phone } = newEmployee;
+
+    const onInputChange = (e) => {
+        setNewEmployee({ ...newEmployee, [e.target.name]: e.target.value })
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        addEmployee(name, email, address, phone);
+    }
+
     return (
-        <Form>
-            <Form.Group>
+        <Form onSubmit={handleSubmit}>
+            <Form.Group className='mb-3'>
                 <Form.Control
                     type='text'
                     placeholder='Name *'
+                    name='name'
+                    value={name}
+                    onChange={e => onInputChange(e)}
                     required
                 />
             </Form.Group>
-            <Form.Group>
+            <Form.Group className='mb-3'>
                 <Form.Control
                     type='email'
                     placeholder='Email *'
+                    name='email'
+                    value={email}
+                    onChange={e => onInputChange(e)}
                     required
                 />
             </Form.Group>
-            <Form.Group>
+            <Form.Group className='mb-3'>
                 <Form.Control
                     as='textarea'
                     placeholder='Address *'
+                    name='address'
+                    value={address}
+                    onChange={e => onInputChange(e)}
                     rows={3}
                 />
             </Form.Group>
-            <Form.Group>
+            <Form.Group className='mb-3'>
                 <Form.Control
                     type='text'
                     placeholder='Phone *'
+                    name='phone'
+                    value={phone}
+                    onChange={e => onInputChange(e)}
                 />
             </Form.Group>
-            <Button variant="success" type="submit" block>
+            <Button variant="success" type="submit">
                 Add New Employee
             </Button>
         </Form>
