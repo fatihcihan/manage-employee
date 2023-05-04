273692572967740af0b088ed65ecc400d29dd23a
Fatih <fatihcihan666@gmail.com>
2023-05-04 12:08:17 +0300

    Added modal for editing and made necessary configuration

--- src/components/Employee.js
+++ src/components/Employee.js
@@ -1,9 +1,16 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
+import { Button, Modal } from 'react-bootstrap';
 import { EmployeeContext } from "../contexts/EmployeeContext";
+import EditForm from "./EditForm";
 
 const Employee = ({ employee }) => {
 
     const { deleteEmployee } = useContext(EmployeeContext);
+
+    const [show, setShow] = useState(false);
+
+    const handleClose = () => setShow(false)
+    const handleShow = () => setShow(true);
 
     return (
         <>
@@ -12,9 +19,25 @@ const Employee = ({ employee }) => {
             <td>{employee.address}</td>
             <td>{employee.phone}</td>
             <td>
-                <button className="btn text-warning btn-act" data-toggle="modal"><i className="material-icons" data-toggle="tooltip" title="Edit">&#xE254;</i></button>
+                <button onClick={handleShow} className="btn text-warning btn-act" data-toggle="modal"><i className="material-icons" data-toggle="tooltip" title="Edit">&#xE254;</i></button>
                 <button onClick={() => deleteEmployee(employee.id)} className="btn text-danger btn-act" data-toggle="modal"><i className="material-icons" data-toggle="tooltip" title="Delete">&#xE872;</i></button>
             </td>
+
+            <Modal show={show} onHide={handleClose}>
+                <Modal.Header className='modal-header' closeButton>
+                    <Modal.Title>
+                        Edit Employee
+                    </Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <EditForm />
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button variant='secondary' onClick={handleClose}>
+                        Close Modal
+                    </Button>
+                </Modal.Footer>
+            </Modal>
         </>
     )
 }
