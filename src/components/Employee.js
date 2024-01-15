c930165cc10fffd2d6c3f390cf2c943a1f3e43c9
Fatih <fatihcihan666@gmail.com>
2024-01-16 00:26:25 +0300

    jsx structure created with a static sample data

--- src/components/Employee.js
+++ src/components/Employee.js
@@ -1,10 +1,15 @@
 const Employee = () => {
     return (
-        <ul>
-            <li>Employee 1</li>
-            <li>Employee 2</li>
-            <li>Employee 3</li>
-        </ul>
+        <tr>
+            <td>Thomas Hardy</td>
+            <td>thomashardy@mail.com</td>
+            <td>89 Chiaroscuro Rd, Portland, USA</td>
+            <td>(171) 555-2222</td>
+            <td>
+                <a href="#editEmployeeModal" className="edit" data-toggle="modal"><i className="material-icons" data-toggle="tooltip" title="" data-original-title="Edit"></i></a>
+                <a href="#deleteEmployeeModal" className="delete" data-toggle="modal"><i className="material-icons" data-toggle="tooltip" title="" data-original-title="Delete"></i></a>
+            </td>
+        </tr>
     )
 }
 
