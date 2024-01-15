9cc0c0bcc9159374e8ec1c899520ad12980096a9
Fatih <fatihcihan666@gmail.com>
2024-01-16 01:36:57 +0300

    fixed bug with rendering employee data properly in map function and dynamicized the data

--- src/components/Employee.js
+++ src/components/Employee.js
@@ -3,18 +3,18 @@ const Employee = ({ employees }) => {
     return (
         <>
             {
-                employees.map((employee, index) => {                
-                    <tr key={index}>
-                        <td>Thomas Hardy</td>
-                        <td>thomashardy@mail.com</td>
-                        <td>89 Chiaroscuro Rd, Portland, USA</td>
-                        <td>(171) 555-2222</td>
+                employees.map((employee, index) => (
+                    <tr key={employee.id}>
+                        <td>{employee.name}</td>
+                        <td>{employee.email}</td>
+                        <td>{employee.address}</td>
+                        <td>{employee.phone}</td>
                         <td>
                             <a href="#editEmployeeModal" className="edit" data-toggle="modal"><i className="material-icons" data-toggle="tooltip" title="" data-original-title="Edit"></i></a>
                             <a href="#deleteEmployeeModal" className="delete" data-toggle="modal"><i className="material-icons" data-toggle="tooltip" title="" data-original-title="Delete"></i></a>
                         </td>
                     </tr>
-                })
+                ))
             }
         </>
     )
