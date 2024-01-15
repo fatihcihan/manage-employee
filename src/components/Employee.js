de9197fd9419de569357a38d2426678225bb501c
Fatih <fatihcihan666@gmail.com>
2024-01-16 01:13:59 +0300

    in Employee component, employee data received as props was rendered with map function

--- src/components/Employee.js
+++ src/components/Employee.js
@@ -1,15 +1,22 @@
-const Employee = () => {
+const Employee = ({ employees }) => {
+    console.log(employees);
     return (
-        <tr>
-            <td>Thomas Hardy</td>
-            <td>thomashardy@mail.com</td>
-            <td>89 Chiaroscuro Rd, Portland, USA</td>
-            <td>(171) 555-2222</td>
-            <td>
-                <a href="#editEmployeeModal" className="edit" data-toggle="modal"><i className="material-icons" data-toggle="tooltip" title="" data-original-title="Edit"></i></a>
-                <a href="#deleteEmployeeModal" className="delete" data-toggle="modal"><i className="material-icons" data-toggle="tooltip" title="" data-original-title="Delete"></i></a>
-            </td>
-        </tr>
+        <>
+            {
+                employees.map((employee, index) => {                
+                    <tr key={index}>
+                        <td>Thomas Hardy</td>
+                        <td>thomashardy@mail.com</td>
+                        <td>89 Chiaroscuro Rd, Portland, USA</td>
+                        <td>(171) 555-2222</td>
+                        <td>
+                            <a href="#editEmployeeModal" className="edit" data-toggle="modal"><i className="material-icons" data-toggle="tooltip" title="" data-original-title="Edit"></i></a>
+                            <a href="#deleteEmployeeModal" className="delete" data-toggle="modal"><i className="material-icons" data-toggle="tooltip" title="" data-original-title="Delete"></i></a>
+                        </td>
+                    </tr>
+                })
+            }
+        </>
     )
 }
 
