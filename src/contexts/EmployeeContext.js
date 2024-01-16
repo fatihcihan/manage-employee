da8edcd8e7596af79500142b921bc393597362bd
Fatih <fatihcihan666@gmail.com>
2024-01-16 23:38:15 +0300

    created with initial values of useState and shared employee data with value prop in EmployeeContext.Provider

--- src/contexts/EmployeeContext.js
+++ src/contexts/EmployeeContext.js
@@ -1,11 +1,20 @@
-import { createContext } from 'react';
+import { createContext, useState } from 'react';
 
 export const EmployeeContext = createContext();
 
-const EmployeeContextProvider = () => {
-
+const EmployeeContextProvider = (props) => {
+    console.log(props,'props');
+    const [employees] = useState([
+        { id: 1, name: 'Thomas Hardy', email: 'thomashardy@mail.com', address: '89 Chiaroscuro Rd, Portland, USA', phone: '(171) 555-2222' },
+        { id: 2, name: 'Dominique Perrier', email: 'dominiqueperrier@mail.com', address: 'Obere Str. 57, Berlin, Germany', phone: '(313) 555-5735' },
+        { id: 3, name: 'Maria Anders', email: 'mariaanders@mail.com', address: '25, rue Lauriston, Paris, France', phone: '(503) 555-9931' },
+        { id: 4, name: 'Fran Wilson', email: 'franwilson@mail.com', address: 'C/ Araquil, 67, Madrid, Spain', phone: '(204) 619-5731' },
+        { id: 5, name: 'Martin Blank', email: 'martinblank@mail.com', address: 'Via Monte Bianco 34, Turin, Italy', phone: '(480) 631-2097' }]);
     return (
-        <>
-        </>
+        <EmployeeContext.Provider value={{employees}}>
+            {props.children}
+        </EmployeeContext.Provider>
     );
 }
+
+export default EmployeeContextProvider;
