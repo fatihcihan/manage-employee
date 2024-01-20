c26acca5fa0ccffe80309d76adc6838176408db3
Fatih <fatihcihan666@gmail.com>
2024-01-20 19:01:40 +0300

    added "mb-3" class to Form.Group component

--- src/components/AddForm.js
+++ src/components/AddForm.js
@@ -8,34 +8,36 @@ const AddForm = () => {
 
     return (
         <Form>
-            <Form.Group>
+            <Form.Group className="mb-3">
                 <Form.Control
                     type="text"
                     placeholder="Name *"
-                    required
-                />
+                    required />
             </Form.Group>
-            <Form.Group>
+
+            <Form.Group className="mb-3">
                 <Form.Control
                     type="email"
                     placeholder="Email *"
-                    required
-                />
+                    required />
             </Form.Group>
-            <Form.Group>
+
+            <Form.Group className="mb-3">
                 <Form.Control
                     type="textarea"
                     placeholder="Address *"
-                    rows={3}
-                />
+                    rows={3} />
             </Form.Group>
             <Form.Group>
                 <Form.Control
+                    className="mb-3"
                     type="text"
                     placeholder="Phone"
                 />
             </Form.Group>
-            <Button variant="success" type="submit">Add New Employee</Button>
+            <Button
+                variant="success"
+                type="submit">Add New Employee</Button>
         </Form>
     )
 }
