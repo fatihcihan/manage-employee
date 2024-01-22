e614d1935dcaa75f4ef7b72a2adee041b08a5a42
Fatih <fatihcihan666@gmail.com>
2024-01-22 19:23:00 +0300

    removed map function and took a single employee as props

--- src/components/Employee.js
+++ src/components/Employee.js
@@ -1,26 +1,20 @@
 import { EmployeeContext } from '../contexts/EmployeeContext';
 import { useContext } from 'react';
 
-const Employee = ({ employees }) => {
+const Employee = ({ employee }) => {
 
     const { deleteEmployee } = useContext(EmployeeContext);
 
     return (
         <>
-            {
-                employees.map((employee) => (
-                    <tr key={employee.id}>
-                        <td>{employee.name}</td>
-                        <td>{employee.email}</td>
-                        <td>{employee.address}</td>
-                        <td>{employee.phone}</td>
-                        <td>
-                            <button className="btn text-warning btn-act" data-toggle="modal"><i className="material-icons" data-toggle="tooltip" title="" data-original-title="Edit"></i></button>
-                            <button onClick={() => deleteEmployee(employee.id)} className="btn text-danger btn-act" data-toggle="modal"><i className="material-icons" data-toggle="tooltip" title="" data-original-title="Delete"></i></button>
-                        </td>
-                    </tr>
-                ))
-            }
+            <td>{employee.name}</td>
+            <td>{employee.email}</td>
+            <td>{employee.address}</td>
+            <td>{employee.phone}</td>
+            <td>
+                <button className="btn text-warning btn-act" data-toggle="modal"><i className="material-icons" data-toggle="tooltip" title="" data-original-title="Edit"></i></button>
+                <button onClick={() => deleteEmployee(employee.id)} className="btn text-danger btn-act" data-toggle="modal"><i className="material-icons" data-toggle="tooltip" title="" data-original-title="Delete"></i></button>
+            </td>
         </>
     )
 }
