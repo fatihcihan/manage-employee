c025e213983e1e47315d7304dfa41895b6746e3c
Fatih <fatihcihan666@gmail.com>
2024-01-23 14:36:52 +0300

    Created a state for each form element, added value value and onChange event to these form elements. Created handleSubmit function for the form

--- src/components/EditForm.js
+++ src/components/EditForm.js
@@ -1,18 +1,35 @@
 import { Form, Button } from 'react-bootstrap';
 import { EmployeeContext } from '../contexts/EmployeeContext';
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 
-const EditForm = () => {
+const EditForm = ({ theEmployee }) => {
 
     const { updateEmployee } = useContext(EmployeeContext);
 
+    const employee = theEmployee;
+    const id = employee.id;
+
+    const [name, setName] = useState(employee.name);
+    const [email, setEmail] = useState(employee.email);
+    const [address, setAddress] = useState(employee.address);
+    const [phone, setPhone] = useState(employee.phone);
+
+    const updatedEmployee = { id, name, email, address, phone }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        updateEmployee(id, updatedEmployee);
+    }
+
     return (
-        <Form >
+        <Form onSubmit={handleSubmit} >
             <Form.Group className="mb-3">
                 <Form.Control
                     type="text"
                     placeholder="Name *"
                     name="name"
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
                     required />
             </Form.Group>
 
@@ -21,6 +38,8 @@ const EditForm = () => {
                     type="email"
                     placeholder="Email *"
                     name="email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
                     required />
             </Form.Group>
 
@@ -29,6 +48,8 @@ const EditForm = () => {
                     type="textarea"
                     placeholder="Address *"
                     name="address"
+                    value={address}
+                    onChange={(e) => setAddress(e.target.value)}
                     as="textarea" rows={3} />
             </Form.Group>
             <Form.Group className="mb-3">
@@ -36,6 +57,8 @@ const EditForm = () => {
                     type="text"
                     placeholder="Phone"
                     name="phone"
+                    value={phone}
+                    onChange={(e) => setPhone(e.target.value)}
                 />
             </Form.Group>
             <Button
