a79856d6d7f994016c6c0dde119ed93891fd3a20
Fatih <fatihcihan666@gmail.com>
2024-01-23 20:11:37 +0300

    sorting operation in context is received in the comment line

--- src/contexts/EmployeeContext.js
+++ src/contexts/EmployeeContext.js
@@ -11,6 +11,9 @@ const EmployeeContextProvider = (props) => {
         { id: uuidv4(), name: 'Fran Wilson', email: 'franwilson@mail.com', address: 'C/ Araquil, 67, Madrid, Spain', phone: '(204) 619-5731' },
         { id: uuidv4(), name: 'Martin Blank', email: 'martinblank@mail.com', address: 'Via Monte Bianco 34, Turin, Italy', phone: '(480) 631-2097' }]);
 
+    // const sortedEmployees = employees.sort((a, b) => (a.name < b.name ? -1 : 1));
+    
+
     const addEmployee = (name, email, address, phone) => {
         setEmployees([...employees, { id: uuidv4(), name, email, address, phone }]);
     }
