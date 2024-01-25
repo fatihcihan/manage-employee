1f997193d68f9d9ae9fe98981bf2df24c27bb455
Fatih <fatihcihan666@gmail.com>
2024-01-25 14:58:44 +0300

    fixed class attribute to className in JSX

--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -1,15 +1,15 @@
 const Pagination = () => {
     return (
-        <div class="clearfix">
-            <div class="hint-text">Showing <b>5</b> out of <b>25</b> entries</div>
-            <ul class="pagination">
-                <li class="page-item disabled"><a href="#">Previous</a></li>
-                <li class="page-item"><a href="#" class="page-link">1</a></li>
-                <li class="page-item"><a href="#" class="page-link">2</a></li>
-                <li class="page-item active"><a href="#" class="page-link">3</a></li>
-                <li class="page-item"><a href="#" class="page-link">4</a></li>
-                <li class="page-item"><a href="#" class="page-link">5</a></li>
-                <li class="page-item"><a href="#" class="page-link">Next</a></li>
+        <div className="clearfix">
+            <div className="hint-text">Showing <b>5</b> out of <b>25</b> entries</div>
+            <ul className="pagination">
+                <li className="page-item disabled"><a href="#">Previous</a></li>
+                <li className="page-item"><a href="#" className="page-link">1</a></li>
+                <li className="page-item"><a href="#" className="page-link">2</a></li>
+                <li className="page-item active"><a href="#" className="page-link">3</a></li>
+                <li className="page-item"><a href="#" className="page-link">4</a></li>
+                <li className="page-item"><a href="#" className="page-link">5</a></li>
+                <li className="page-item"><a href="#" className="page-link">Next</a></li>
             </ul>
         </div>
     );
